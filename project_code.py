fd60d19461634bba8912828f8a577658d42b2364
CharlotteElle <36274357+CharlotteElle@users.noreply.github.com>
2018-02-09 10:37:58 +0000

    Add files via upload
    Had a chat with Oliver. Panda's binned

--- project_code.py
+++ project_code.py
@@ -6,20 +6,11 @@ Created on Wed Feb  7 05:47:02 2018
 """
 
 import numpy as np
-import pandas as pd
 
 
 sfile = 'C:\\Users\\Charlotte\\Documents\\Uni\\Physics\\Project\\Tachometer\\Data_capture\\capture3_fullrun_nostamp.txt'
 tp = np.genfromtxt(sfile,dtype=np.str, delimiter=20)
 print(tp)
-
-
-
-
-
-df = pd.read_csv(sfile, sep=' ', header=None)
-df.info()
-print(df)
 
 
 
